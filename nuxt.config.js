98480554c39038556640d26dd9ee90d607a2491f
Sebastian Landwehr <info@dword-design.de>
2021-03-05 02:33:45 +0100

    init

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+import dotenv from '@dword-design/dotenv-json-extended'
+
+dotenv.config()
+
+export default {
+  modules: [
+    '@nuxtjs/axios',
+    ['nuxt-mail', {
+      smtp: JSON.parse(process.env.MAIL_CONFIG),
+    }],
+  ],
+}
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <label for="email">Your email address:</label>
+    <input id="email" type="email" v-model="email" />
+    <label for="message">Message:</label>
+    <textarea id="message" v-model="message" />
+    <button type="submit" @click.prevent="send">Send email</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: '',
+    message: '',
+  }),
+  methods: {
+    send() {
+      this.$mail.send({
+        from: this.email,
+        subject: 'Contact form message',
+        text: this.message,
+        to: 'me@gmail.com',
+      })
+    }
+  }
+}
+</script>
